2394cc4cd79136dff4e83680058c699b5f7d121c
shailesh <diwanjishailesh@gmail.com>
2025-05-26 14:35:05 +0530

    added ngrxStore functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './shared/counter.reducer';
+import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component';
+import { CounterdisplayComponent } from './component/counterdisplay/counterdisplay.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './material.module';
+import { CustomeCounterComponent } from './component/custome-counter/custome-counter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterbuttonComponent,
+    CounterdisplayComponent,
+    CustomeCounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({counter:counterReducer}),
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/counter.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from "@ngrx/store";
+
+export const increament=createAction('increament')
+export const decreament=createAction('decreament')
+export const reset=createAction('reset')
+export const customIncrement=createAction('customincrement',props<{value:number}>())
+
+// export const customIncrement = createAction(
+//     ' Custom Increment',
+//     props<{ value: number }>()
+//   );
