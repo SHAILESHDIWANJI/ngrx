5be3a38cfa69d7b11b55d9c65d522b94b5a1f1b0
shailesh <diwanjishailesh@gmail.com>
2025-06-01 10:05:43 +0530

    delete functionality added

--- src/app/shared/store/Blog/blog.actions.ts
+++ src/app/shared/store/Blog/blog.actions.ts
@@ -6,3 +6,4 @@ export const loadBlog=createAction("loadBlog")
 
 export const adblog=createAction('adblog',props<{bloginput:BlogModel}>())
 export const updateblog=createAction('updateblog',props<{bloginput:BlogModel}>())
+export const deleteblog=createAction('deleteblog',props<{id:number}>())
--- src/app/shared/store/Blog/blog.reducers.ts
+++ src/app/shared/store/Blog/blog.reducers.ts
@@ -1,6 +1,7 @@
 import { createReducer, on } from "@ngrx/store";
 import { BlogState } from "./blog.state";
-import { adblog, loadBlog } from "./blog.actions";
+import { adblog, deleteblog, loadBlog, updateblog } from "./blog.actions";
+import { BlogModel } from "./blog.model";
 
 const _blogReducer = createReducer(
 BlogState,
@@ -12,10 +13,31 @@ BlogState,
   }),
   on(adblog, (state,action) => {
     const _blog={...action.bloginput}
-    _blog.id=state.blogList.length
+    _blog.id=state.blogList.length+1
     return {
       ...state,
       blogList:[...state.blogList,_blog]
+   
+    };
+  }),
+  on(updateblog, (state,action) => {
+    const _blog={...action.bloginput}
+    const updateblog=state.blogList.map(blog=>{
+      return _blog.id==blog.id?_blog:blog
+    })
+    return {
+      ...state,
+      blogList:updateblog
+   
+    };
+  }),
+  on(deleteblog, (state,action) => {
+    const updateblog=state.blogList.filter((data:BlogModel)=>{
+      return data.id!==action.id
+    })
+    return {
+      ...state,
+      blogList:updateblog
    
     };
   }),
